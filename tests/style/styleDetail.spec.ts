6ae278f91b9551f333d367ffbfef0c0a1366386d
prakaifa21 <prakaifa@konsys.co>
2026-04-21 15:53:09 +0700

    remove styledetail file

--- tests/style/styleDetail.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from "@playwright/test";
-import { styleDatas } from "../../interfaces/style.interface";
-import { StylePage } from "../../page/style.page";
-
-export const creatStype = async (page: Page, styleData: styleDatas, createdStyleNo?: string) => {
-    const stylePage = new StylePage(page);
-    await stylePage.expectStyleDetail(styleData, createdStyleNo ?? styleData.styleName);
-};
